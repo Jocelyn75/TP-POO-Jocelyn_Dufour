e21bf994068ac87e23950f87837728a3ccf8008a
Jocelyn Dufour <jocelyn.dufour@outlook.com>
2023-12-11 17:29:41 +0100

    Code final fonctionnel

--- Fight.php
+++ Fight.php
@@ -11,22 +11,33 @@ class Fight
         $challengerStamina = $challenger->getStamina();
 
         for($i=0; $i<10; $i++)
-        {    
+        {   
+            if(rand(1,10) === 1)
+            {
+                $challengerStamina = $challengerStamina - $ippo->getStrength() * 2;
+                echo "Coup critique pour Ippo ! \n";
+            }
+
             if($ippo->getSpeed() > $challenger->getSpeed())
                 
             $challengerStamina = $challengerStamina - $ippo->getStrength();
             echo "Points de vie restants de Challenger : $challengerStamina \n";
 
-            if($ippoStamina < 0 )
+            if($ippoStamina <= 0 )
                 {
                     return "Le combat est terminé, Challenger a gagné";
                 }
-            if($challengerStamina < 0)
+            if($challengerStamina <= 0)
                 {
                     return "Le combat est terminé, Ippo a gagné";
                 }
             else
             {
+                if(rand(1,10) === 2)
+                {
+                    $ippoStamina = $ippoStamina - $challenger->getStrength() * 2;
+                    echo "Coup critique pour Challenger ! \n";
+                }
                 $ippoStamina = $ippoStamina - $challenger->getStrength();
                 echo "Points de vie restants d'Ippo : $ippoStamina \n";
             }
